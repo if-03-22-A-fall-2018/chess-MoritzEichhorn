560147205c7b3f91f8b6f8a408bfe76bcd9f7eda
Moritz Eichhorn <moritz.eichhorn@outlook.com>
2018-11-06 11:45:15 +0100

    Added code

--- assignment/chess.h
+++ assignment/chess.h
@@ -10,3 +10,22 @@
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+
+ struct ChessPiece{
+   enum PieceType type;
+   enum PieceColor color;
+ };
+
+ struct ChessSquare{
+   bool is_occupied;
+   struct ChessPiece piece;
+
+ };
+
+ enum PieceType {
+   Pawn, Rook, Knight, Bishop, Queen, King, NoPiece
+ };
+ enum PieceColor { White, Black };
+ enum MoveType { NormalMove, CaptureMove };
+
+ typedef struct ChessSquare ChessBoard[8][8];
